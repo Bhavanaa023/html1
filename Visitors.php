bbe376c0fd71b9637583c38a627e7cfcb2db7883
Bhavanaa023 <bhavana.23cs023@sode-edu.in>
2025-11-13 12:55:25 +0530

    PHP program with HTML/CSS to give number of visitors visiting web page

---
+++ Visitors.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>visitor Counter</title>
+    <style>
+        body{
+            font-family: Arial,sans-serif;
+            text-align: center;
+            margin-top: 50px;
+        }
+        h1{
+            color: #333;
+        }
+        p{
+            font-size: 1.2em;
+        }
+    </style>
+</head>
+<body>
+    <?php   
+        $file='counter.txt';
+        $count=file_exists($file) ? (int)file_get_contents($file):0;
+        file_put_contents($file,++$count);
+    ?>
+    <h1> visitor Counter</h1>
+    <p> Number of Visitors: <strong> <?php echo $count; ?> </strong> </p>
+</body>
+</html>
